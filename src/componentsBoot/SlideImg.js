35e7e2baa3bebe7879aea189af0eaf8b4ef19527
Jay Kumar <jay.k.992000@gmail.com>
2025-06-21 01:30:53 +0530

    Initial commit for React eCommerce

---
+++ src/componentsBoot/SlideImg.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./SlideImg.css";
+
+const SlideImg = () => {
+
+return (
+
+<div>
+<div className="img_flex">
+<div></div>
+{/*  */}
+</div>
+{/*  */}
+<div className="img_flex">
+<div></div>
+{/*  */}
+</div>
+</div>
+
+);
+};
+
+export default SlideImg;
